c20f19989d74ec5d78cb1a9b6df71d96f98d8088
nbirsan <nbirsan@hoyoslabs.com>
2015-05-02 22:42:53 +0300

    modificat

---
+++ solutions/04-landmarklister/src/ro/pub/cs/systems/pdsd/lab07/landmarklister/controller/grstge.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.pdsd.lab07.landmarklister.controller;
+
+public class grstge {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
